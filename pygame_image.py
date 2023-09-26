84af77518617d2ca4ea7c6b6da9ca05f2c073db3
TakuD1D <c0a2205191@edu.teu.ac.jp>
2023-09-26 15:21:50 +0900

    3question

--- pygame_image.py
+++ pygame_image.py
@@ -7,9 +7,12 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex01/fig/3.png")  # 追加
+    kk_img = pg.image.load("ex01/fig/3.png")
+    kk_img_list = []
+    kk_img_list.append(kk_img)
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_img_list.append(kk_img)
     tmr = 0
     while True:
         for event in pg.event.get():
